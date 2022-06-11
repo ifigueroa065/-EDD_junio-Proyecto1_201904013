7f3e8f4b0a282376011de5465e3ded6d426a0fef
Roafnisma <isaiimiff13@gmail.com>
2022-06-11 01:40:19 -0600

    update

--- ESTRUCTURAS/ORTOGONAL/Ortogonal.js
+++ ESTRUCTURAS/ORTOGONAL/Ortogonal.js
@@ -1,234 +1,121 @@
-class Nodo {
-    constructor(n) {
-        this.n = n
-        this.siguiente = null
-        this.anterior = null
+class Node{
+    constructor(pix){
+        this.pix=pix;
+        this.top=null;
+        this.bot=null;
+        this.left=null;
+        this.right=null
+    }
+    setdato(pix){
+        this.pix=pix
     }
 }
 
-class ListaDobleCircular{
-    constructor() {
-        this.primero = null
-        this.ultimo = null
-        this.tam=0
-    }
-
-    add(digito) {
-        var nuevo = new Nodo(digito)
-
-        if (this.primero==null) {
-            this.primero=nuevo
-            this.primero.siguiente=this.primero
-            this.primero.anterior=this.ultimo
-            this.ultimo=nuevo
-            this.tam++
-        }else{
-            nuevo.anterior=this.ultimo
-            this.ultimo.siguiente=nuevo
-            nuevo.siguiente=this.primero
-            this.ultimo=nuevo
-            this.primero.anterior=this.ultimo
-            this.tam++
-        }
-
-
-    }
-
-    mostrar(){
-        var temporal = this.primero
-        var cont =0;
-        while(cont<this.tam){
-            console.log(temporal.n)
-            temporal = temporal.siguiente
-            cont++;
-        }
-        
-    }
-
-    mostrar_alreves(){
-        
-        var temporal= this.ultimo
-
-        while (temporal!=this.primero) {
-            console.log(temporal.n)
-            temporal=temporal.anterior
-        }
-        console.log(temporal.n)
+class L_Ortogonal {
     
+    constructor(){
+        this.cabeza=null
+        this.cont=0
     }
 
-    graficar(){
-        
-        
-        var codigodot = "digraph G {\n"
-        codigodot +="node[ style=filled ,color=\"#E1E1A8\", shape=box];";
-        codigodot +="label=\"" + "RECORRIDO INICO A FIN" + "\";\n";
-        var temporal = this.primero
-        var conexiones ="";
-        var nodos ="";
-        var numnodo= 0;
-        var cont =0;
-        while (cont<this.tam) {
-            
-            nodos+=  "N" + numnodo + "[label=\"" + temporal.n + "\" ];\n"
-            if(temporal.siguiente==this.ultimo){
-                var auxnum = numnodo+1
-                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
-                
-            }else if(temporal.siguiente != this.primero){
-                var auxnum = numnodo+1
-                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
-                conexiones += "N" + auxnum + " -> N" + numnodo + ";\n"
+    estaVacia(){
+        return this.cabeza==null
+    }
 
-            }else{
-                var auxnum = 0
-                var auxnum2= numnodo-1
-                conexiones += "N" + auxnum + " -> N" + numnodo + ";\n"
-                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
-                conexiones += "N" + numnodo + " -> N" + auxnum2 + ";\n"
+    agregarNodo(pix,fila){
+        if (this.estaVacia()) {
+            var  tempNodo= new Node(pix)
+            this.cabeza=tempNodo
+            this.cont++
+        } else {
+            var aux= this.cabeza
+
+            //recorrido vertical
+            while (aux.bot!=null) {
+                aux=aux.bot
             }
-            temporal = temporal.siguiente
-            numnodo++;
-            cont++;  
-                      
-        }
-        
-        codigodot += "//agregando nodos\n"
-        codigodot += nodos+"\n"
-        codigodot += "//agregando conexiones o flechas\n"
-        codigodot += "{rank=same;\n"+conexiones+"\n}\n}"
-        console.log(codigodot)
-        
-        d3.select("#lienzo").graphviz()
-            .width(1800)
-            .height(200)
-            .renderDot(codigodot)
-    }
 
-    graficar_alreves(){
-        
-        
-        var codigodot = "digraph G {\n"
-        codigodot +="node[ style=filled ,color=\"#E1E1A8\", shape=box];";
-        codigodot +="label=\"" + "RECORRIDO FIN A INICIO" + "\";\n";
-        var temporal = this.ultimo
-        var conexiones ="";
-        var nodos ="";
-        var numnodo= 0;
-        var cont =0;
-        while (temporal!=this.primero) {
+            if (this.cont!=fila) {
+                this.cont++
+                var tempNodo= new Node(pix)
+
+                //creando una nueva fila
+                aux.bot=tempNodo    //apunta abajo
+                tempNodo.top=aux    //apunta arriba
+            } else {
+                //recorrido horizontal
+                while (aux.right!=null) {
+                    aux=aux.right
+                }
             
-            nodos+=  "N" + numnodo + "[label=\"" + temporal.n + "\" ];\n"
-            if(temporal.anterior==this.ultimo){
-                var auxnum = numnodo+1
-                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
+
+                //agregando nodo en una fila    
+                var tempNodo= new Node(pix)
+                aux.right=tempNodo
+                tempNodo.left=aux
+
+                //si el nodo es de una fila <> a la principal
+                if (this.cont>1) {
+                    //se crea 2do nodo auxiliar
+                    var aux2=aux.top.right
+                    aux2.bot=tempNodo
+                    tempNodo.top=aux2
+                }
                 
-            }else if(temporal.anterior != this.primero){
-                var auxnum = numnodo+1
-                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
-                conexiones += "N" + auxnum + " -> N" + numnodo + ";\n"
-
             }
-            temporal = temporal.anterior
-            numnodo++;
-            cont++;  
-                      
         }
-        nodos+=  "N" + numnodo + "[label=\"" + temporal.n + "\" ];\n"
-        var auxnum = numnodo-1
-        var auxo=0
-        conexiones += "N" + auxnum + " -> N" + numnodo + ";\n"
-        conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
-
-        conexiones += "N" + numnodo + " -> N" + auxo + ";\n"
-        conexiones += "N" + auxo + " -> N" + numnodo + ";\n"
-
-
-        codigodot += "//agregando nodos\n"
-        codigodot += nodos+"\n"
-        codigodot += "//agregando conexiones o flechas\n"
-        codigodot += "{rank=same;\n"+conexiones+"\n}\n}"
-        console.log(codigodot)
-        
-        d3.select("#lienzo2").graphviz()
-            .width(1800)
-            .height(200)
-            .renderDot(codigodot)
     }
 
-    graficar_doble(){
-        
-        
-        var codigodot = "digraph G {\n"
-        codigodot +="node[ style=filled ,color=\"#E1E1A8\", shape=box];";
-        codigodot +="label=\"" + "DOBLE RECORRIDO" + "\";\n";
-        var temporal = this.primero
-        var conexiones ="";
-        var nodos ="";
-        var numnodo= 0;
-        var cont =0;
-        while (cont<this.tam) {
-            
-            nodos+=  "N" + numnodo + "[label=\"" + temporal.n + "\" ];\n"
-            if(temporal.siguiente==this.ultimo){
-                var auxnum = numnodo+1
-                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
-                
-            }else if(temporal.siguiente != this.primero){
-                var auxnum = numnodo+1
-                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
-                conexiones += "N" + auxnum + " -> N" + numnodo + ";\n"
-
-            }else{
-                var auxnum = 0
-                var auxnum2= numnodo-1
-                conexiones += "N" + auxnum + " -> N" + numnodo + ";\n"
-                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
-                conexiones += "N" + numnodo + " -> N" + auxnum2 + ";\n"
+    mostrarLista(){
+        var contcol=1
+        var contfila=1
+        var aux = this.cabeza
+        var img = ""
+        while (aux.bot!=null || aux.right!=null) {
+            img+=aux.pix
+            if (aux.right!=null) {
+                aux=aux.right
+                contcol++
+            } else {
+                contfila++
+                contcol=1
+                //imprimo la fila que leí
+                console.log(img)
+                if (aux.bot!=null) {
+                    img=""
+                    aux=aux.bot
+                    while (aux.left!=null) {
+                        aux=aux.left
+                    }
+                }
             }
-            temporal = temporal.siguiente
-            numnodo++;
-            cont++;  
-                      
         }
-        
-        
-        codigodot += "//agregando nodos\n"
-        codigodot += nodos+"\n"
-        codigodot += "//agregando conexiones o flechas\n"
-        codigodot += "{rank=same;\n"+conexiones+"\n}\n}"
-        console.log(codigodot)
-        
-        d3.select("#lienzo3").graphviz()
-            .width(1800)
-            .height(200)
-            .renderDot(codigodot)
+        img+=aux.pix
+        console.log(img)
+
     }
+
 }
 
-var LDC = new ListaDobleCircular();
-LDC.add(2)
-LDC.add(0)
-LDC.add(1)
-LDC.add(9)
-LDC.add(0)
-LDC.add(4)
-LDC.add(0)
-LDC.add(1)
-LDC.add(3)
+const prueba= new L_Ortogonal
+prueba.agregarNodo("#",1)
+prueba.agregarNodo("#",1)
+prueba.agregarNodo("#",1)
+prueba.agregarNodo("#",1)
 
-LDC.mostrar()
-LDC.graficar()
-console.log("________________")
-LDC.mostrar_alreves()
-LDC.graficar_alreves()
-LDC.add(2)
-LDC.add(0)
-LDC.add(1)
-LDC.add(9)
-LDC.add(0)
-LDC.add(4)
-LDC.add(0)
-LDC.add(1)
-LDC.add(3)
-LDC.graficar_doble()
+prueba.agregarNodo("-",2)
+prueba.agregarNodo("#",2)
+prueba.agregarNodo("-",2)
+prueba.agregarNodo("#",2)
+
+prueba.agregarNodo("#",3)
+prueba.agregarNodo("#",3)
+prueba.agregarNodo("#",3)
+prueba.agregarNodo("#",3)
+
+prueba.agregarNodo("#",4)
+prueba.agregarNodo("#",4)
+prueba.agregarNodo("#",4)
+prueba.agregarNodo("#",4)  
+
+prueba.mostrarLista()
