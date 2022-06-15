456452c049327b8056efe331059659964db62cc6
Roafnisma <isaiimiff13@gmail.com>
2022-06-15 14:03:51 -0600

    matriz dispersa

--- Backend/Estructuras/DISPERSA/Dispersa.js
+++ Backend/Estructuras/DISPERSA/Dispersa.js
@@ -1,234 +1,228 @@
-class Nodo {
-    constructor(n) {
-        this.n = n
+class NodoM {
+    constructor(x,y,dato) {
+        //punteros
         this.siguiente = null
         this.anterior = null
+        this.abajo = null
+        this.arriba = null
+        //data
+        this.dato=dato
+        this.x=x
+        this.y=y
     }
+    nodo_str(){
+        return "("+str(this.x)+","+str(this.y)+")="+str(this.dato)
+    }
+        
 }
 
-class ListaDobleCircular{
+class Dispersa{
     constructor() {
-        this.primero = null
-        this.ultimo = null
-        this.tam=0
-    }
-
-    add(digito) {
-        var nuevo = new Nodo(digito)
-
-        if (this.primero==null) {
-            this.primero=nuevo
-            this.primero.siguiente=this.primero
-            this.primero.anterior=this.ultimo
-            this.ultimo=nuevo
-            this.tam++
-        }else{
-            nuevo.anterior=this.ultimo
-            this.ultimo.siguiente=nuevo
-            nuevo.siguiente=this.primero
-            this.ultimo=nuevo
-            this.primero.anterior=this.ultimo
-            this.tam++
-        }
-
-
-    }
-
-    mostrar(){
-        var temporal = this.primero
-        var cont =0;
-        while(cont<this.tam){
-            console.log(temporal.n)
-            temporal = temporal.siguiente
-            cont++;
-        }
+        this.root  = new NodoM(-1,-1,"Root")
         
     }
 
-    mostrar_alreves(){
-        
-        var temporal= this.ultimo
-
-        while (temporal!=this.primero) {
-            console.log(temporal.n)
-            temporal=temporal.anterior
+    //Buscar columna
+    buscar_columna(x){
+        var aux=this.root
+        while (aux!=null) {
+            if (aux.x==x) {
+                return aux
+            }
+            aux=aux.siguiente
         }
-        console.log(temporal.n)
+        return null
+    }
     
-    }
-
-    graficar(){
-        
-        
-        var codigodot = "digraph G {\n"
-        codigodot +="node[ style=filled ,color=\"#E1E1A8\", shape=box];";
-        codigodot +="label=\"" + "RECORRIDO INICO A FIN" + "\";\n";
-        var temporal = this.primero
-        var conexiones ="";
-        var nodos ="";
-        var numnodo= 0;
-        var cont =0;
-        while (cont<this.tam) {
-            
-            nodos+=  "N" + numnodo + "[label=\"" + temporal.n + "\" ];\n"
-            if(temporal.siguiente==this.ultimo){
-                var auxnum = numnodo+1
-                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
-                
-            }else if(temporal.siguiente != this.primero){
-                var auxnum = numnodo+1
-                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
-                conexiones += "N" + auxnum + " -> N" + numnodo + ";\n"
-
-            }else{
-                var auxnum = 0
-                var auxnum2= numnodo-1
-                conexiones += "N" + auxnum + " -> N" + numnodo + ";\n"
-                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
-                conexiones += "N" + numnodo + " -> N" + auxnum2 + ";\n"
+    buscar_fila(y){
+        var aux=this.root
+        while (aux!=null) {
+            if (aux.y==y) {
+                return aux
             }
-            temporal = temporal.siguiente
-            numnodo++;
-            cont++;  
-                      
+            aux=aux.abajo
         }
-        
-        codigodot += "//agregando nodos\n"
-        codigodot += nodos+"\n"
-        codigodot += "//agregando conexiones o flechas\n"
-        codigodot += "{rank=same;\n"+conexiones+"\n}\n}"
-        console.log(codigodot)
-        
-        d3.select("#lienzo").graphviz()
-            .width(1800)
-            .height(200)
-            .renderDot(codigodot)
+        return null
     }
 
-    graficar_alreves(){
-        
-        
-        var codigodot = "digraph G {\n"
-        codigodot +="node[ style=filled ,color=\"#E1E1A8\", shape=box];";
-        codigodot +="label=\"" + "RECORRIDO FIN A INICIO" + "\";\n";
-        var temporal = this.ultimo
-        var conexiones ="";
-        var nodos ="";
-        var numnodo= 0;
-        var cont =0;
-        while (temporal!=this.primero) {
-            
-            nodos+=  "N" + numnodo + "[label=\"" + temporal.n + "\" ];\n"
-            if(temporal.anterior==this.ultimo){
-                var auxnum = numnodo+1
-                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
-                
-            }else if(temporal.anterior != this.primero){
-                var auxnum = numnodo+1
-                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
-                conexiones += "N" + auxnum + " -> N" + numnodo + ";\n"
+  
 
+
+    //Insertar Cabecera Columna
+    crear_columna(x){
+        var nodo_col =this.root
+        var nuevo = new NodoM(x,-1,"COL")
+        var columna= this.insertar_orden_col(nuevo,nodo_col)
+        return columna
+    }
+    
+
+    //Insertar en orden columnas
+    insertar_orden_col(nuevo,cabeza_col){
+        var aux = cabeza_col
+        var insertado = new Boolean(false)
+        while (true) {
+            if (nuevo.x==aux.x) {
+                //si la posicion ya existe sobre escribimos
+                aux.y=nuevo.y
+                aux.dato=nuevo.dato
+                return aux
+            }else if (aux.x>nuevo.x){
+                //insertar en medio, antes del aux
+                insertado=true
+                break
             }
-            temporal = temporal.anterior
-            numnodo++;
-            cont++;  
-                      
-        }
-        nodos+=  "N" + numnodo + "[label=\"" + temporal.n + "\" ];\n"
-        var auxnum = numnodo-1
-        var auxo=0
-        conexiones += "N" + auxnum + " -> N" + numnodo + ";\n"
-        conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
 
-        conexiones += "N" + numnodo + " -> N" + auxo + ";\n"
-        conexiones += "N" + auxo + " -> N" + numnodo + ";\n"
-
-
-        codigodot += "//agregando nodos\n"
-        codigodot += nodos+"\n"
-        codigodot += "//agregando conexiones o flechas\n"
-        codigodot += "{rank=same;\n"+conexiones+"\n}\n}"
-        console.log(codigodot)
-        
-        d3.select("#lienzo2").graphviz()
-            .width(1800)
-            .height(200)
-            .renderDot(codigodot)
-    }
-
-    graficar_doble(){
-        
-        
-        var codigodot = "digraph G {\n"
-        codigodot +="node[ style=filled ,color=\"#E1E1A8\", shape=box];";
-        codigodot +="label=\"" + "DOBLE RECORRIDO" + "\";\n";
-        var temporal = this.primero
-        var conexiones ="";
-        var nodos ="";
-        var numnodo= 0;
-        var cont =0;
-        while (cont<this.tam) {
-            
-            nodos+=  "N" + numnodo + "[label=\"" + temporal.n + "\" ];\n"
-            if(temporal.siguiente==this.ultimo){
-                var auxnum = numnodo+1
-                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
-                
-            }else if(temporal.siguiente != this.primero){
-                var auxnum = numnodo+1
-                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
-                conexiones += "N" + auxnum + " -> N" + numnodo + ";\n"
-
-            }else{
-                var auxnum = 0
-                var auxnum2= numnodo-1
-                conexiones += "N" + auxnum + " -> N" + numnodo + ";\n"
-                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
-                conexiones += "N" + numnodo + " -> N" + auxnum2 + ";\n"
+            if (aux.siguiente != null) {
+                aux=aux.siguiente
+            } else {
+                //insertar despues del auxiliar
+                insertado=false
+                break
             }
-            temporal = temporal.siguiente
-            numnodo++;
-            cont++;  
-                      
         }
-        
-        
-        codigodot += "//agregando nodos\n"
-        codigodot += nodos+"\n"
-        codigodot += "//agregando conexiones o flechas\n"
-        codigodot += "{rank=same;\n"+conexiones+"\n}\n}"
-        console.log(codigodot)
-        
-        d3.select("#lienzo3").graphviz()
-            .width(1800)
-            .height(200)
-            .renderDot(codigodot)
+        if (insertado) {
+            nuevo.siguiente=aux
+            aux.anterior.siguiente=nuevo
+            nuevo.anterior=aux.anterior
+            aux.anterior=nuevo
+        } else {
+            aux.siguiente=nuevo
+            nuevo.anterior=aux
+        }
+        return nuevo
     }
-}
+    
 
-var LDC = new ListaDobleCircular();
-LDC.add(2)
-LDC.add(0)
-LDC.add(1)
-LDC.add(9)
-LDC.add(0)
-LDC.add(4)
-LDC.add(0)
-LDC.add(1)
-LDC.add(3)
+    //nsertar Cabecera Fila
+    crear_fila(y){
+        var nodo_fila= this.root
+        var nuevo = new NodoM(-1,y,"Fila")
+        var columna = this.insertar_orden_fil(nuevo,nodo_fila)
+        return columna
+    }
 
-LDC.mostrar()
-LDC.graficar()
-console.log("________________")
-LDC.mostrar_alreves()
-LDC.graficar_alreves()
-LDC.add(2)
-LDC.add(0)
-LDC.add(1)
-LDC.add(9)
-LDC.add(0)
-LDC.add(4)
-LDC.add(0)
-LDC.add(1)
-LDC.add(3)
-LDC.graficar_doble()
+    //Insertar en orden Filas
+    insertar_orden_fil(nuevo,cabeza_fila){
+        var aux = cabeza_fila
+        var insertado= new Boolean(false)
+        while(true){
+            if (nuevo.y==aux.y) {
+                //si la posicion ya existe sobre escribimos
+                aux.x=nuevo.x
+                aux.dato=nuevo.dato
+                return aux
+            }else if (aux.y>nuevo.y){
+                //insertar en medio, antes del aux
+                insertado=true
+                break
+            }
+
+            if (aux.abajo != null) {
+                aux=aux.abajo
+            } else {
+                //insertar despues del auxiliar
+                insertado=false
+                break
+            }
+        }
+        if (insertado) {
+            nuevo.abajo=aux
+            aux.arriba.abajo=nuevo
+            nuevo.arriba=aux.arriba
+            aux.arriba=nuevo
+        } else {
+            aux.abajo=nuevo
+            nuevo.arriba=aux
+        }
+        return nuevo
+        }
+
+        //Metodo para insertar  
+    insertarNodo(x,y,dato){
+        var nuevo = new NodoM(x,y,dato)
+        var NodoColumna = this.buscar_columna(x)
+        var NodoFila =this.buscar_fila(y)
+
+        
+        if (NodoFila==null && NodoColumna==null) {
+            //Caso 1---No existe fila ni columna
+            console.log("Caso1, ---No existe fila ni columna")
+
+            //crear las cabeceras
+            NodoColumna=this.crear_columna(x)
+            NodoFila=this.crear_fila(y)
+
+            //insertamos contenido
+            nuevo=this.insertar_orden_col(nuevo,NodoFila)
+            nuevo=this.insertar_orden_fil(nuevo,NodoColumna)
+            return
+        }else if(NodoFila==null && NodoColumna!=null){
+            //Caso 2---No existe Fila, pero si columna
+            console.log("Caso2")
+
+            //crear las cabeceras
+            //NodoColumna=this.crear_columna(x)
+            NodoFila=this.crear_fila(y)
+
+            //insertamos contenido
+            nuevo=this.insertar_orden_col(nuevo,NodoFila)
+            nuevo=this.insertar_orden_fil(nuevo,NodoColumna)
+            return
+        }else if(NodoFila!=null && NodoColumna==null){
+            //Caso 3---No existe columna, pero si fila
+            console.log("Caso3")
+
+            //crear las cabeceras
+            NodoColumna=this.crear_columna(x)
+            //NodoFila=self.crear_fila(y)
+            
+            //insertamos contenido
+            nuevo=this.insertar_orden_col(nuevo,NodoFila)
+            nuevo=this.insertar_orden_fil(nuevo,NodoColumna)
+            return
+        }else if(NodoFila!=null && NodoColumna!=null){
+            //Caso 4 ---  si existe fila y columna
+
+            console.log("Caso4")
+            
+            //crear las cabeceras
+            //NodoColumna=self.crear_columna(x)
+            //NodoFila=self.crear_fila(y)
+            
+            //insertamos contenido
+            nuevo=this.insertar_orden_col(nuevo,NodoFila)
+            nuevo=this.insertar_orden_fil(nuevo,NodoColumna)
+            return
+        }
+    }
+    
+    imprimir(){
+        var aux =this.root
+        while(aux!=null){
+            var text = ""
+            var aux2 = aux
+            while(aux2!=null){
+                text+= "["+aux2.x + aux2.y+"]"
+                aux2=aux2.siguiente
+            }
+            console.log(text)
+            aux=aux.abajo
+        }
+        return null
+    }
+
+    }
+
+    
+    
+
+
+var matrizAux=new Dispersa()
+matrizAux.insertarNodo(0,0,"hola")
+matrizAux.insertarNodo(1,1,"hola")
+matrizAux.insertarNodo(2,2,"hola")
+matrizAux.insertarNodo(3,3,"hola")
+matrizAux.insertarNodo(1,3,"hola")
+matrizAux.insertarNodo(5,1,"hola")
+matrizAux.imprimir()
